ad7a8ea2900a55de4417053f019d82444442aa8b
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-01-26 15:42:06 -0300

    a

--- Dashboard.py
+++ Dashboard.py
@@ -58,7 +58,7 @@ with tab2:
 with tab3:
     st.subheader("Link Para Preencher",anchor=False)
     st.image("img/qr.png")
-    components.iframe(src=frame, width=800, height=600)
+    st.write(components.iframe(src=frame, width=800, height=600))
 
 
 # ---------------------------------------------------------------------------------------------
