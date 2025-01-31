3aa27a8aa73ba03664abdf46b7ad863ecdaedf12
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-01-31 20:32:59 -0300

    a

--- Dashboard.py
+++ Dashboard.py
@@ -451,9 +451,9 @@ with colmap:
     carro = st.selectbox("Carro",df_Veiculos)
     df_filtrado = df.query('Veículo == @carro')
     
-mapa = df_filtrado["Latitude","Longitude"]
+mapa = df_filtrado[["Latitude", "Longitude"]] 
 
-mapa = df_filtrado.loc[:, ["Latitude", "Longitude"]]  # Outra forma correta!
+
 
 
 # mapa = folium.Map(location=[-23.567, -46.633], zoom_start=6,tiles="CartoDB Dark_Matter")
