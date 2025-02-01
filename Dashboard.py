a0a8a19d7411d9146340e6fe432c9b40fe4e8150
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-02-01 17:43:31 -0300

    a

--- Dashboard.py
+++ Dashboard.py
@@ -135,7 +135,7 @@ with tab1:
 
         if contagem_saidas2 % 2 == 0:
             st.write(f"🟠 Veículo A")
-            st.write(df_carro.loc[ultimo_indice, "Origem" - "Destino"])
+            st.write(df_carro.loc[ultimo_indice, "Origem" "-" "Destino"])
             st.image("img/Busout.png",width=180)
             st.write(f'{df_carro.loc[ultimo_indice, "Motorista:"]}')
 
