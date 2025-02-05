5b0e27156b52e8e407fdfa79c06cbc0b461d384a
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-02-04 21:02:52 -0300

    a

--- Dashboard.py
+++ Dashboard.py
@@ -413,10 +413,10 @@ with card3:
     st.metric("Garagem",f'🟡 {patio}')
 
 # -------------------------------------------------------------------------------------
-df = df.set_index("index")  # Define 'index' como índice
-df_unico = df.loc[df.reset_index()  
-                    .drop_duplicates(subset='Veiculo', keep='last')  # Remove duplicatas, mantendo o último
-                    .set_index("index").index]  # Restaura o índice original
+# df = df.set_index("index")  # Define 'index' como índice
+# df_unico = df.loc[df.reset_index()  
+#                     .drop_duplicates(subset='Veiculo', keep='last')  # Remove duplicatas, mantendo o último
+#                     .set_index("index").index]  # Restaura o índice original
 
 
 # -------------------------------------------------------------------------------------
